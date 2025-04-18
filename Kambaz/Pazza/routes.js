0eb7a688a5ad9f78e3f2a2c22c971cd3d204bc8d
Ryan Cheung <cheung.ry@northeastern.edu>
2025-04-18 15:48:15 -0400

    more fp

--- Kambaz/Pazza/routes.js
+++ Kambaz/Pazza/routes.js
@@ -1,4 +1,5 @@
 import * as dao from "./dao.js";
+import model from "./model.js";
 
 export default function PazzaRoutes(app) {
     app.post("/api/pazza/posts", async (req, res) => {
@@ -16,6 +17,18 @@ export default function PazzaRoutes(app) {
         const posts = await dao.findPostsByCourse(cid);
         res.json(posts);
     });
+    app.put("/api/pazza/posts/:pid", async (req, res) => {
+        const { pid } = req.params;
+        const updated = await model.findByIdAndUpdate(pid, req.body, { new: true });
+        res.json(updated);
+    });
+
+    app.delete("/api/pazza/posts/:pid", async (req, res) => {
+        const { pid } = req.params;
+        const status = await model.deleteOne({ _id: pid });
+        res.json(status);
+    });
+
 
 }
 
